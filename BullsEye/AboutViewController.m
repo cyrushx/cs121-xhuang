f1723edfd963e29f00a57e4ef855c69ed85f2f49
Tingyu <apple@wl-216-61.wpa.claremont.edu>
2014-09-07 14:50:19 -0700

    4-inch works

--- BullsEye/AboutViewController.m
+++ BullsEye/AboutViewController.m
@@ -26,6 +26,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString *htmlFile = [[NSBundle mainBundle]
+                          pathForResource:@"BullsEye" ofType:@"html"]; NSData *htmlData = [NSData dataWithContentsOfFile:htmlFile];
+    NSURL *baseURL = [NSURL fileURLWithPath:[
+                                             [NSBundle mainBundle] bundlePath]];
+    [self.webView loadData:htmlData MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:baseURL];
     // Do any additional setup after loading the view.
 }
 
@@ -38,6 +44,9 @@
 - (IBAction)close {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (BOOL)prefersStatusBarHidden {
+    return YES; }
 
 /*
 #pragma mark - Navigation
